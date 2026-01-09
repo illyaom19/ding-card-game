4e7039437e57adf6e78e6c7c389748878e1756b3
Illya Omelyanchuk <illyaom19@gmail.com>
2026-01-08 18:07:11 -0800

    Began Multiplayer integration

---
+++ firebase-config.example.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
